d520af8e0f09e678cc9f1075f806f386cef80118
roflganker <abominati0n@protonmail.com>
2023-10-31 14:34:42 +0000

    Added comments for configuration

--- src/faucet/faucet.config.ts
+++ src/faucet/faucet.config.ts
@@ -9,17 +9,33 @@ import {
 
 @Injectable()
 export class FaucetConfig implements OnModuleInit {
+  /**
+   * ## Address of deployed Faucet smart-contract
+   */
   @IsEthereumAddress()
   public readonly contractAddress = process.env.FAUCET_CONTRACT_ADDRESS;
 
+  /**
+   * ## Private key of account sending transaction
+   *
+   * Generally, any account with enough gas
+   */
   @IsString()
   @Length(66, 66)
   public readonly workerPk = process.env.FAUCET_WORKER_PK;
 
+  /**
+   * ## Private key of account, signing dispension proofs
+   *
+   * **trusted signer** in context of a faucet
+   */
   @IsString()
   @Length(66, 66)
   public readonly signerPk = process.env.FAUCET_SIGNER_PK;
 
+  /**
+   * Salt needed to protect identities (ips) from revealing
+   */
   @IsString()
   @IsNotEmpty()
   public readonly identitySalt = process.env.FAUCET_IDENTITY_SALT;
